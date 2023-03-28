97ffd2b0a0b4011264e404bfa874e99164104e58
Sage Santomenna <chellster1003@gmail.com>
2023-03-27 23:02:28 -0700

    flet a mess atm

--- GUIScheduler.py
+++ GUIScheduler.py
@@ -1,5 +1,16 @@
-import sys
+import sys, os, flet as ft
 from scheduleLib import sCoreCondensed as sc
+from flet import (Column, ElevatedButton, FilePicker, FilePickerResultEvent, FilePickerUploadEvent, FilePickerUploadFile, Page, ProgressRing, Ref, Row, Text, icons)
+
+def expandPath(workingDir, returner):
+    if not os.path.isdir(workingDir):  # we're a file. append ourselves and return!
+        returner.append(workingDir)
+        return returner
+    else:  # call our nodes
+        for item in os.listdir(workingDir):
+            returner = expandPath(workingDir + "/" + item, returner)
+        return returner
+
 schedule = sc.readSchedule("files/exampleSchedule.txt")
 print(schedule.summarize())
 
@@ -7,7 +18,7 @@ print(schedule.summarize())
 #[2] preferred min time between observations, [3] preferred minimum time between observations of the same object, [4] start time in the format DD-MM-YYYY HH:MM:SS
 if len(sys.argv)<5:
     raise Exception("Not enough arguments provided. Please provide the path to a folder with lists of ephemerides for each object (filename should be [object name].txt), preferred min time between observations (minutes), preferred minimum time between observations of the same object (minutes), and start time in the format DD-MM-YYYY HH:MM:SS")
-ephemsDir, minTimeBetween, minTimeBetweenSame, startTime = sys.argv[1:5]
+# ephemsDir, minTimeBetween, minTimeBetweenSame, startTime = sys.argv[1:5]
 startTime = datetime.strptime(startTime, "%d-%m-%Y %H:%M:%S")
 
 
@@ -64,7 +75,7 @@ class scheduleBuilder:
         obs = obsPack.obs
         obs.startTime, obs.endTime = obsStartTime, obsStartTime + relativedelta(seconds = float(obs.duration))
         schedule.appendTask(obs)
-        self.currentTime = obs.endTime + relativedleta(minutes = float(minTimeBetween))
+        self.currentTime = obs.endTime + relativedelta(minutes = float(minTimeBetween))
     def deleteObservation(self,obs): #deletes the given observation and all observations after it - probably better to use some graph structure here but whatever
         index = self.schedule.tasks.index(obs)
         while self.schedule.tasks[index]:
@@ -87,16 +98,188 @@ class scheduleBuilder:
             if nextObs == {}:
                 break
             else:
-                nextToPlace = presentOptions(nextObs)
-                placeObservation(nextToPlace, nextToPlace.obs.startTime)
+                nextToPlace = self.presentOptions(nextObs)
+                self.placeObservation(nextToPlace, nextToPlace.obs.startTime)
     def presentOptions(self, nextObs): #this is where the GUI stuff will go
-        #this function will build a GUI that shows the user the next observation candidates and let them choose which one to place
-        #it will return the observation package that the user chooses
-        #implement the solution for now using the console
         for obj in nextObs.keys():
             print("Possible observation of "+obj+" at "+nextObs[obj].obs.startTime)
         print("Please enter the name of the object you would like to observe next.")
         choice = input()
         return nextObs[choice]
 
+###### GUI ########
+#using flet ("ft"), python port of flutter
+#colors
+blue = ft.colors.BLUE_400
+red = ft.colors.RED_400
+green = ft.colors.GREEN_400
+orange = ft.colors.ORANGE_800
+yellow = ft.colors.YELLOW_400
+
+# styles
+titleStyle = lambda c: ft.TextStyle(color=c, fontSize=30, fontWeight=ft.FontWeight. BOLD)
+headerStyle = lambda c: ft.TextStyle(color=c, fontSize=20, fontWeight=ft.FontWeight. BOLD)
+bodyStyle = lambda c: ft.TextStyle(color=c, fontSize=15, fontWeight=ft.FontWeight.normal)
+buttonStyle = lambda c: ft.ButtonStyle(color=c, fontSize=15, fontWeight=ft.FontWeight.BOLD)
+
+def main(sch,page: Page):
+
+    # variables ####
+
+    nextObs = []
+    candidateObsObjects={}
+    placedObsObjects={}
+
+    ephemsDir = ""
+    potentialObs = ""
+    selectedFiles = ft.Text(value="No files selected", style=headerStyle(blue))
+
+    # functions ###
+
+    def onDirectoryChosen(e: ft.FilePickerResultEvent):
+        selectedFiles.value = (
+            ", ".join(expandPath(e.files[0].path,[])) if e.files else "Cancelled!"
+        )
+        # fileList.join(expandPath(e.files[0].path,[]))
+        selectedFiles.update()
+        ephemsDir = e.files[0].path
+    # def onFilesChosen(e: ft.FilePickerResultEvent):
+    #     #append all the paths to fileList
+    #     selectedFiles.value = selectedFiles.value.join([i.path for i in e.files] if e.files else "Cancelled!")
+    #     fileList.join([i.path for i in e.files])
+    #     selectedFiles.update()
+
+    def loadEphems():
+        filePicker = ft.get_directory_path(on_result=onDirectoryChosen)
+        potentialObs = loadPotentialObs(ephemsDir, startTime)
+        page.update()
+    # def addEphems():
+    #     filePicker = ft.get_directory_path(on_result=onFilesChosen)
+    #     potentialObs loadPotentialObs(ephemsDir, startTime)
+    #     page.update()
+
+    def placeObs(obsPack):
+        obj = placedObservation(obsPack)
+        scheduleList.controls.append(obj)
+        sch.scheduleBuilder.placeObservation(obsPack, obsPack.obs.startTime)
+        nextObs = sch.nextObservationCandidates(potentialObs, sch.currentTime)
+        page.update()
+
+
+        # title box is a horizontal container with the title text and four buttons: Import Folder, Help, Credits, Settings
+    titleBox = ft.Row(
+        [
+            ft.Text(value="The Scheduler", style=titleStyle(blue)),
+            ft.Button(value="Import Folder", style=buttonStyle(blue), on_click=loadEphems),
+            ft.Button(value="Help", style=buttonStyle(blue)),
+            ft.Button(value="Credits", style=buttonStyle(blue)),
+            ft.Button(value="Settings", style=buttonStyle(blue)),
+        ],
+        mainAxisAlignment=ft.MainAxisAlignment.spaceBetween,
+        crossAxisAlignment=ft.CrossAxisAlignment.left,
+    )
+    page.add(titleBox)
+
+    # a placedObservation is a container with the object name, start time, duration, and a button to remove it from the schedule. this is a function to create one from an obsPack object
+    def placedObservation(obsPack):
+        return ft.Container(
+            [
+                ft.Text(value=obsPack.obs.target.name, style=bodyStyle(blue)),
+                ft.Text(value=obsPack.obs.startTime, style=bodyStyle(blue)),
+                ft.Text(value=obsPack.obs.duration, style=bodyStyle(blue)),
+                ft.Button(value="Delete", style=buttonStyle(red)),
+            ],
+            data = obsPack,
+            mainAxisAlignment=ft.MainAxisAlignment.spaceBetween,
+            crossAxisAlignment=ft.CrossAxisAlignment.left,
+        )
+
+    def observationCandidate(obsPack):
+        return ft.Container(
+            [
+                ft.Text(value=obsPack.obs.target.name, style=bodyStyle(blue)),
+                ft.Text(value=obsPack.obs.startTime, style=bodyStyle(blue)),
+                ft.Text(value=obsPack.obs.duration, style=bodyStyle(blue)),
+                ft.Button(value="Place", style=buttonStyle(green), on_click=placeObs(obsPack)),
+            ],
+            data = obsPack,
+            mainAxisAlignment=ft.MainAxisAlignment.spaceBetween,
+            crossAxisAlignment=ft.CrossAxisAlignment.left,
+        )
+
+
+    #schedule box is a vertical container with the schedule title, a container holding each entry of the schedule, and a control box at the bottom containing buttons "Export" "Analyze" "Undo", and a trash icon button
+    scheduleList = ft.ListView(expand=True, data = [], spacing=10)
+
+    scheduleBox = ft.Column(
+        [
+            ft.Text(value="Schedule:", style=headerStyle(blue)),
+            scheduleList,
+            ft.Row(
+                [
+                    ft.Button(value="Export", style=buttonStyle(green)),
+                    ft.Button(value="Analyze", style=buttonStyle(blue)),
+                    ft.Button(value="Undo", style=buttonStyle(yellow)),
+                    ft.Button(value="Delete", style=buttonStyle(red)),
+                ],
+                mainAxisAlignment=ft.MainAxisAlignment.spaceBetween,
+                crossAxisAlignment=ft.CrossAxisAlignment.center,
+            ),
+        ],
+        mainAxisAlignment=ft.MainAxisAlignment.start,
+        crossAxisAlignment=ft.CrossAxisAlignment.start
+    )
+
+
+    #to the right of scheduleBox is a box called "Choose Next" that displays a horizontal list of possible observations, with a button to place each one
+    chooseNextList = ft.ListView(expand=True, data = [], spacing=10)
+    chooseNextBox = ft.Column(
+        [
+            ft.Text(value="Choose Next", style=headerStyle(blue)),
+            chooseNextList
+        ]
+    )
+
+    #the loadedFiles box is a vertical container to the right of schedule box with the title "Loaded Files" and a list of the files loaded into the program, half as tall as the other boxes
+    loadedFiles = ft.ListView(expand=True, data = [], spacing=10)
+    loadedFilesBox = ft.Column(
+        [
+            ft.Text(value="Loaded Files", style=headerStyle(blue)),
+            loadedFiles
+        ]
+    )
+
+    # the loaded objects box is below the loaded files box, with the title "Loaded Objects" and a list of the objects loaded from the files
+    loadedObjects = ft.ListView(expand=True, data = [], spacing=10)
+    loadedObjectsBox = ft.Column(
+        [
+            ft.Text(value="Loaded Objects", style=headerStyle(blue)),
+            loadedObjects
+        ]
+    )
+
+    loadedBox = ft.Column([loadedFilesBox, loadedObjectsBox])
+
+    # the windowsBox is a horizontal grid with the next observation window on the left and the candidate observation window on the right, then to the right is the loaded objects box at the top and the loaded files box at the bottom
+    windowsBox = ft.Row(scheduleBox, chooseNextBox, loadedBox, spacing=10)
+    page.add(windowsBox)
+
+
+
+    
+    page.controls.append(ft.Text(value="Possible Observations:", style=titleStyle))
+    for obj in nextObs.keys():
+        page.controls.append(ft.Text(value="Possible observation of " + obj + " at " + nextObs[obj].obs.startTime, style=bodyStyle))
+        page.controls.append(ft.ElevatedButton(text="Place observation", on_click=buttonClicked))
+
+    t = ft.Text(value="Hello, world!", color="green")
+    page.controls.append(t)
+    page.add(ft.ElevatedButton(text="Click me", on_click=lambda a:page.add(ft.Text("Clicked!"))))     #page.add is a shortcut for page.controls.append and page.update in one line
+    page.update()
+
+
+
+ft.app(target=main)
+#running from command line like as follows allows hot reloading
+os.system('flet run firstFlet.py -d')
 
---
+++ firstFlet.py
@@ -0,0 +1,152 @@
+import sys,os, flet as ft
+
+#control = widget
+# controls can contain other controls
+
+#refs are bassically forward declarations for controls - you can create a variable and have it store a ref. you set the ref to a control later, and then the ref can be used to access the control.
+# #this is useful for things like buttons, where you want to be able to access the button later to change its text or something
+#access later with [ref name].current.[field] = [new value]
+# #for example, if you want to change the text of a button, you can do something like this:
+# #button_ref.current.text = "New text"
+#on_click adds a handler
+# page.update() batches changes and only updates things that have changed
+# has some builtin icons - ft.IconButton(ft.icons.[icon name])
+#can use autofocus to set focus to a control automatically and to move from one to the other
+
+#if displaying a large list/grid of items, use listview/gridview to improve performance
+
+#import fonts:
+# https://flet.dev/docs/controls/page/#fonts
+
+
+# def buttonClicked(e):
+#     page.add(ft.Text("Clicked!"))
+#
+# # can also add keyboard shortcuts to controls
+# def onKeyboard(e: ft.KeyboardEvent):
+#     page.add(
+#         ft.Text(
+#             f"Key: {e.key}, Shift: {e.shift}, Control: {e.ctrl}, Alt: {e.alt}, Meta: {e.meta}"
+#         )
+#     )
+# page.on_keyboard_event = onKeyboard # bind the on_keyboard function to the page
+#
+# t = ft.Text(value="Hello, world!", color="green")
+# page.controls.append(t)
+# page.add(ft.ElevatedButton(text="Click me", on_click=lambda a:page.add(ft.Text("Clicked!"))))     #page.add is a shortcut for page.controls.append and page.update in one line
+
+# schedule box is a vertical container with the schedule title, a container holding each entry of the schedule, and a control box at the bottom containing buttons "Export" "Analyze" "Undo", and a trash icon button
+
+# here's an example of a flet ElevatedButton with a blue background and white text:
+
+
+
+
+
+
+
+
+
+def main(page: ft.Page):
+    # using flet ("ft"), python port of flutter
+    # colors
+    blue = ft.colors.BLUE_400
+    red = ft.colors.RED_400
+    green = ft.colors.GREEN_400
+    orange = ft.colors.ORANGE_800
+    yellow = ft.colors.YELLOW_400
+    white = ft.colors.WHITE
+
+    page.bgcolor = white
+    # styles
+    titleText = lambda t,c: ft.Text(t, color=c, style=ft.TextThemeStyle.DISPLAY_MEDIUM, weight=ft.FontWeight.BOLD)
+    print(titleText("hello",blue))
+    headerText = lambda t,c: ft.Text(t, color=c, style=ft.TextThemeStyle.HEADLINE_LARGE, weight=ft.FontWeight.BOLD)
+    bodyText = lambda t,c: ft.Text(t, color=c, style=ft.TextThemeStyle.BODY_LARGE, weight=ft.FontWeight.NORMAL)
+    buttonStyle = lambda c: ft.ButtonStyle(color=ft.colors.WHITE,bgcolor=c)
+
+    def containerize(content):
+        return ft.Container(content)
+    titleBox = ft.Row(
+        controls=[
+            titleText("The Scheduler", blue),
+            containerize(ft.ElevatedButton(text="Import Folder", style=buttonStyle(blue))),
+            ft.ElevatedButton(text="Help", style=buttonStyle(blue)),
+            ft.ElevatedButton(text="Credits", style=buttonStyle(blue)),
+            ft.ElevatedButton(text="Settings", style=buttonStyle(blue)),
+        ],
+        spacing=20
+    )
+    # page.add(titleBox)
+
+    #use a container to add a border to titleBox:
+    titleBoxContainer = ft.Container(titleBox, border=ft.border.all(2, blue), border_radius = ft.border_radius.all(5), margin=ft.margin.all(10), padding=ft.padding.all(10))
+    page.add(titleBoxContainer)
+    scheduleList = ft.ListView(expand=True, controls=[containerize(bodyText("No Data",blue))], spacing=10)
+    scheduleButtonRow = containerize(ft.Row(
+        controls=[
+                   ft.ElevatedButton(text="Export", style=buttonStyle(green)),
+                   ft.ElevatedButton(text="Analyze", style=buttonStyle(blue)),
+                   ft.ElevatedButton(text="Undo", style=buttonStyle(yellow)),
+                   ft.ElevatedButton(text="Delete", style=buttonStyle(red))
+                ],
+                spacing=20
+            ))
+
+    col = ft.Column(spacing=20, controls= [containerize(headerText("Schedule", blue)),containerize(scheduleList), containerize(scheduleButtonRow)])
+    scheduleContainer = ft.Container(col, border=ft.border.all(2, blue), border_radius=10, margin=10, padding=10)
+    # scheduleBox = ft.container(content=ft.Column(
+    #     controls=[
+    #         headerText("Schedule", blue),
+    #         scheduleList,
+    #         scheduleButtonRow
+    #     ],
+    #     alignment=ft.MainAxisAlignment.START,
+    #     spacing=20
+    # ))
+    # page.add(scheduleBox)
+
+    page.add(scheduleContainer)
+
+    # to the right of scheduleBox is a box called "Choose Next" that displays a horizontal list of possible observations, with a button to place each one
+    chooseNextList = ft.ListView(expand=True, data=[], spacing=10)
+    chooseNextBox = ft.Column(
+        controls=[
+            containerize(headerText("Choose Next", blue)),
+            containerize(chooseNextList)
+        ]
+    )
+
+    # the loadedFiles box is a vertical container to the right of schedule box with the title "Loaded Files" and a list of the files loaded into the program, half as tall as the other boxes
+    loadedFiles = ft.ListView(expand=True, data=[], spacing=10)
+    loadedFilesBox = ft.Column(
+        controls=[
+            headerText("Loaded Files", blue),
+            loadedFiles
+        ]
+    )
+
+    # the loaded objects box is below the loaded files box, with the title "Loaded Objects" and a list of the objects loaded from the files
+    loadedObjects = ft.ListView(expand=True, data=[], spacing=10)
+    loadedObjectsBox = ft.Column(
+        controls=[
+            headerText("Loaded Objects", blue),
+            loadedObjects
+        ]
+    )
+
+    loadedBox = ft.Column(controls=[loadedFilesBox, loadedObjectsBox])
+    page.add(chooseNextBox)
+
+    page.add(loadedFilesBox)
+
+    # page.add(loadedBox)
+    # the windowsBox is a horizontal grid with the next observation window on the left and the candidate observation window on the right, then to the right is the loaded objects box at the top and the loaded files box at the bottom
+    windowsBox = ft.Row(controls=[(scheduleContainer), (chooseNextBox), (loadedBox)], spacing=10)
+    page.add(windowsBox)
+    page.update()
+
+
+ft.app(target=main)
+#running from command line like as follows allows hot reloading
+os.system('flet run firstFlet.py -d')
