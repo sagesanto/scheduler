c02b9469644425cc41cf42bec5918beb2d65f1ab
sagesanto <chellster1003@gmail.com>
2023-05-22 16:12:40 -0700

    this works but will likely be switching over to use different target retrieval system

--- scheduleLib/getMPC.py
+++ scheduleLib/getMPC.py
@@ -1,5 +1,5 @@
 #---standard
-import json, pandas as pd, time, os, asyncio
+import json, pandas as pd, time, os, asyncio, numpy as np
 
 #---webtools
 import httpx #this and selenium are our main tools for interacting with the web
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup #to parse html files
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 #--- astronomy stuff
 from astropy.time import Time
@@ -18,31 +19,39 @@ from astral import LocationInfo, zoneinfo
 from datetime import datetime, timezone,timedelta
 from astropy.coordinates import Angle
 
-mapClient = httpx.AsyncClient(follow_redirects=True,timeout=45.0) #web client for saving error maps
+offsetClient = httpx.AsyncClient(follow_redirects=True,timeout=45.0) #client for uncertainty retrieval
 
 saveImages = False
 debug = True
-headless = True
+headless = False
 
-ERRORRANGE = 300
+if saveImages:
+    mapClient = httpx.AsyncClient(follow_redirects=True, timeout=45.0)  # web client for saving error maps
+
+ERRORRANGE = 360
 NOBSMAX = 100000000
-VMAGMAX = 21
+VMAGMAX = 21.6
 SCOREMIN = 50
-MAXTRANSIT = 6 #maximum number of hours until object transits
+MAXTRANSIT = 6.75 #maximum number of hours until object transits
 MINTRANSIT = 0 #make negative to allow the object to have already transited
+DECMAX = 65
+DECMIN = -25
+
 
 TMO = LocationInfo(name='TMO', region='CA, USA', timezone='UTC', latitude=34.36, longitude=-117.63)
 sidereal = Time(datetime.utcnow(), scale='utc').sidereal_time(kind="apparent",longitude=TMO.longitude)
+programStartTime = time.time()
 
 #---start the selenium instance---
-opts = webdriver.FirefoxOptions()
+#had to switch from Firefox to Chrome because something (probably a Firefox update an hour previous) prompted selenium to start creating errors
+# opts = webdriver.FirefoxOptions()
+# if headless:
+#     opts.headless = True
+#browser = webdriver.Firefox(options=opts)
 
-programStartTime = time.time()
-if headless:
-    opts.headless = True
-browser = webdriver.Firefox(options=opts)
-
-
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+browser = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
 
 async def saveHTML(url,filename):
     async with httpx.AsyncClient(follow_redirects=True) as client:
@@ -50,7 +59,6 @@ async def saveHTML(url,filename):
     open(filename, 'wb').write(rawFile.content)
 
 async def saveMapImage(url,filePath):
-    global mapClient
     if debug:
         print("Starting async save for",filePath)
     name = filePath[31:]
@@ -58,8 +66,8 @@ async def saveMapImage(url,filePath):
         response = await mapClient.get(url)
     except httpx.TimeoutException:
         print("Timed out on url",url,"which corresponded to",name)
-        open(filePath.replace(name,'')+"SAVE-FAILED-"+name, 'wb').write("The program timed out while trying to fetch this error map, which was last seen at "+url+" - be advised that these links expire quickly.")
-        return("Fail")
+        open(filePath.replace(name,'')+"SAVE-FAILED-"+name+".txt", 'wb').write("The program timed out while trying to fetch this error map, which was last seen at "+url+" - be advised that these links expire quickly.")
+        return tuple("Fail",name)
 
     soup = BeautifulSoup(response.text, 'html.parser')
     image = soup.find('img')
@@ -69,10 +77,13 @@ async def saveMapImage(url,filePath):
         imgResponse = await client.get(image_url)
     img = Image.open(BytesIO(imgResponse.content))
     img.save(filePath)
-    return("Success")
+    return tuple("Success",name)
+
+# def flagTargetRemoved(df,conditionMask,removedReason):
+#     return df.loc[(df['Score'] <= scoreMin)].copy().assign(removed='Score')
+#     df["Desirable"] = np.where(df[row] != '[]', True, False)
 
 def pullNEO():
-    global debug
     mpcJson = httpx.get("https://www.minorplanetcenter.net/Extended_Files/neocp.json").json()
     # print(mpcJson)
     # open('./neocp.json', 'wb').write(mpcJson)
@@ -81,7 +92,6 @@ def pullNEO():
     return mpcDf
 
 def timeUntilTransit(RA):
-    global sidereal
     RA = Angle(str(RA)+"h")
     return (RA-sidereal).hour
 
@@ -89,53 +99,78 @@ def slice(updatedTime):
     return updatedTime[-12:]
 
 def passOne(vMagMax, nObsMax, scoreMin):
-    global NOBSMAX, VMAGMAX, MAXTRANSIT, MINTRANSIT
     df = pullNEO().drop(labels=["Discovery_year", "Discovery_month", "Discovery_day"], axis=1)
-    filtDf = df.loc[(df['Score'] >= scoreMin) & (df['V'] <= vMagMax) & (df['NObs'] <= nObsMax)]
-    filtDf['TransitDiff'] = filtDf.apply(lambda row: timeUntilTransit(row['R.A.']), axis=1)
-    filtDf = filtDf.loc[(filtDf['TransitDiff'] >= MINTRANSIT) & (filtDf['TransitDiff'] <= MAXTRANSIT)]
+    df['TransitDiff'] = df.apply(lambda row: timeUntilTransit(row['R.A.']), axis=1)
+    original = len(df.index)
+    # removed = pd.DataFrame(index=list(df.columns))
+    print("Before pruning, we started with",original)
+
+    conditions = [
+        (df['Score'] <= scoreMin),
+        (df['V'] >= vMagMax),
+        (df['NObs'] >= nObsMax),
+        (df['TransitDiff'] <= MINTRANSIT) | (df['TransitDiff'] >= MAXTRANSIT),
+        (df['Decl.'] <= DECMIN) | (df['Decl.'] >= DECMAX)
+    ]
+    removedReasons = ["score","magnitude","nObs","RA","Declination"]
+    df["removed"] = np.select(conditions,removedReasons)
+    print("df:\n\n",df.to_string())
+    filtDf = df.loc[(df["removed"] == "0")]
+    print("Number desirable targets:",len(filtDf.index))
+
+    for reason in removedReasons:
+        print("Removed", len(df.loc[(df["removed"] == reason)].index), "targets because of their",reason)
+
+    print("In total, removed",len(df.index)-len(filtDf.index),"targets.")
     # if debug:
         # print(filtDf)
         # print(filtDf.columns)
-    # filtDf['Updated'] = filtDf.apply(lambda row: slice(row['Updated']), axis=0)
-    dfSort = filtDf.sort_values(by=["Updated"], ascending=True)
-    return dfSort
+
+    # filtDf['Updated'] = filtDf.apply(lambda row: slice(row['Updated']), axis=1)
+
+    filtDf = filtDf.sort_values(by=["TransitDiff"], ascending=True)
+    return filtDf,df
 
 
 def clickButton(browser, xpath, name, index=None):  # name is just for readability
-    global debug
     if index is not None:
         button = browser.find_elements(By.XPATH,xpath)[index] #note the plural of element
     else:
         button = browser.find_element(By.XPATH, xpath)
-    if debug:
-        print("Clicked", name)
+    # if debug:
+    #     print("Clicked", name)
     button.click()
 
 
-
 def generateEphemerides(designations):
-    global browser
     if headless:
         print("Launching headless browser")
     # get MPC website
     browser.get('https://minorplanetcenter.net/iau/NEO/toconfirm_tabular.html')
     # wait for it to load
     time.sleep(2)
-
     # ---find and check the boxes of all the neos we are interested in---
-    try:
-        i = 0
-        for name in designations:
-            i+=1
+    failed = []
+    i = 0
+    numChecked = 0
+    for name in designations:
+        i += 1
+        try:
             checkbox = browser.find_element(By.XPATH, "//input[@value='" + name + "']")
             if not checkbox.get_dom_attribute("checked") == "true":
-                if debug:
-                    print("[" + str(i) + "] Clicking for", name)
-                checkbox.click()
-    except NoSuchElementException:
-        print("Couldn't find element matching", name)
+                # if debug:
+                #     print("[" + str(i) + "] Clicking for", name)
+                    checkbox.click()
+                    numChecked +=1
+        except NoSuchElementException:
+            print("Couldn't find element matching", name)
+            failed.append(name)
 
+    #remove the ones that failed.
+    for n in failed:
+        designations.remove(n)
+    print("Fetching details for the following targets failed:",failed)
+    print("Checked",numChecked,"targets.",len(designations),"are in the designations list. Hopefully those two numbers are equal.")
     # ---find and fill out the form at the bottom---
     # enter the obsCode
     obsCodeEntry = browser.find_element(By.XPATH, "//input[@name='obscode']")
@@ -146,9 +181,9 @@ def generateEphemerides(designations):
     clickButton(browser, "//input[@name='int']", "10 Minute Ephemeris Interval",2)
     # set dmot
     clickButton(browser, "//input[@name='dmot']", "Separate R.A. and Decl. coordinate motions",1)
-
     # ---submit---
     clickButton(browser, "//input[@value=' Get ephemerides ']", "Submit")
+    return designations
 
 async def asyncOffsetProcessor(offsetDict): #offsetDict will come in {name:offsetUrl} and leave {name:soup}
     print("Beginning asyncOffsetProcessor")
@@ -167,48 +202,41 @@ async def asyncOffsetProcessor(offsetDict): #offsetDict will come in {name:offse
 
 #by the time we call this function, we will have already retrieved the initial ephemeris. what we need to do now is navigate the epehemeris so we are later in a place to prune out the ones with the high uncertainty maps.
 async def navigateEphemerides(designations):
-    global browser
     n = len(designations)
     dir = "OffsetMaps" + datetime.now().strftime("%m_%d_%Y-%H_%M_%S")
     os.mkdir(dir)
     offsetDict = {}
+    mapDict = {}
     startTime = time.time()
-    tasks = []
-
+    failedList = []
     #---now, we're going to try to request each object's offsets and uncertainty map. we'll do this en masse, save the maps, then move on to process the uncertainty responses
     for i in range(n):
+        failed = False
         # identify the object - the object's name is in /html/body/p[n]/b where n starts at 8 and increments by 4
         name = browser.find_element(By.XPATH, "/html/body/p["+str(8+4*i)+"]/b").text
-        if debug:
-            print("Retrieving uncertainty for",name)
         #find the links to the map and offset page for the object
-        mapLink = browser.find_element(By.XPATH, "/html/body/pre["+str(i+1)+"]/a[1]").get_dom_attribute('href')
-        offsetLink = browser.find_element(By.XPATH, "/html/body/pre["+str(i+1)+"]/a[2]").get_dom_attribute('href')
+        try:
+            mapLink = browser.find_element(By.XPATH, "/html/body/pre["+str(i+1)+"]/a[1]").get_dom_attribute('href')
+            offsetLink = browser.find_element(By.XPATH, "/html/body/pre["+str(i+1)+"]/a[2]").get_dom_attribute('href')
+        except NoSuchElementException as e:
+            print("Couldn't find links for "+name+". moving on")
+            if debug:
+                print(name,"had xpath","/html/body/p["+str(8+4*i)+"]/b")
+                print("Exception raised:", e.msg)
+            failed = True
+            failedList.append(name)
 
-        #--- use the links we just found ---
-        #asynchronously save the map as an image for observer use
-        if saveImages:
-            imageTask = asyncio.create_task(saveMapImage(mapLink,dir+"/"+name+"Map.png"))
-            tasks.append(imageTask)
-        else:
-            print("saveImages is False, skipping save step")
-        await mapClient.aclose()
         #store the urls - we'll process these in a moment
-        offsetDict[name] = offsetLink
+        if not failed:
+            offsetDict[name] = offsetLink
+            mapDict[name] = mapLink
 
-    offsetTask = asyncio.create_task(asyncOffsetProcessor(offsetDict))
-    tasks.append(offsetTask)
-
-    returner = await asyncio.gather(*tasks) #this return value stuff is dubious at best
-    # if debug:
-    #     typesList = [type(a) for a in returner]
-    #     print("Returning types:",typesList)
+    offsetDict = await asyncOffsetProcessor(offsetDict)
     timeDifference = time.time() - startTime
-    print(f'Scraping and saving took %.2f seconds.' % timeDifference)
-    return offsetDict
+    print(f'Scraping and reading offsets took %.2f seconds.' % timeDifference)
+    return offsetDict, mapDict, failedList
 
 def extractUncertainty(name, offsetDict):
-    global debug
     if name not in offsetDict.keys():
         if debug:
            print("Couldn't find",name,"in offsetDict")
@@ -223,37 +251,59 @@ def extractUncertainty(name, offsetDict):
     # print("strippedList:",splitList)
     raList = [int(a[0]) for a in splitList]
     decList = [int(a[1]) for a in splitList]
-    print(raList,decList)
     maxRA = max(abs(min(raList)),abs(max(raList)))
     maxDec = max(abs(min(decList)), abs(max(decList)))
 
     return max(maxRA,maxDec)
 
+async def saveMaps(mapDict):
+    tasks = []
+    if saveImages:
+        for name in mapDict.keys():
+            imageTask = asyncio.create_task(saveMapImage(mapDict[name], dir + "/" + name + "Map.png"))
+            tasks.append(imageTask)
+    else:
+        print("saveImages is False, skipping save step")
+        return ["Skip"]
+
+    return await asyncio.gather(*tasks)
+
 ## -- main --
 
-targetDf = passOne(VMAGMAX, NOBSMAX, SCOREMIN)
+targetDf,originalDf = passOne(VMAGMAX, NOBSMAX, SCOREMIN)
 
-
-if debug:
-    print("Unfiltered targets:")
-    print(targetDf.to_string())
+#can graph originalDf obs (that aren't in targetDf) as X's with color by value of "removed" column, then graph filtDf over that
 
 designations = targetDf["Temp_Desig"].to_list()
 print("The following", len(designations), "objects meet the criteria:")
 print(designations)
 
-generateEphemerides(designations)
+availableDesig = generateEphemerides(designations)
+
+print(availableDesig)
+print(("C97XCV2" in availableDesig))
 
 loop = asyncio.get_event_loop()
-offsetDict = loop.run_until_complete(navigateEphemerides(designations))
+offsetDict, mapDict, failedList = loop.run_until_complete(navigateEphemerides(availableDesig))
+
+print("Failed to get uncertainties for",len(failedList),"listed below:")
+print(failedList)
 
 targetDf["Uncertainty"] = targetDf.apply(lambda row: extractUncertainty(row['Temp_Desig'],offsetDict), axis=1)
 
 #filter out the high uncertainty targets. should log somewhere along here! then can use log to detect new targets
-# targetDf =
+targetDf = targetDf.loc[targetDf['Uncertainty'] <= ERRORRANGE]
+outputFilename = "Targets " + datetime.now().strftime("%m_%d_%Y-%H_%M_%S")+".csv"
+targetDf.to_csv(outputFilename,index=False)
+
+mapStatuses = loop.run_until_complete(saveMaps(mapDict))
+if saveImages:
+    mapClient.aclose()
+print("Map statuses:",mapStatuses)
 
 if debug:
     print(" \n\nUncertainties found:\n\n"+targetDf.to_string())
 
 totalDuration = time.time() - programStartTime
+browser.quit()
 print(f'\nCompleted with total runtime of %.2f seconds.' % totalDuration)
